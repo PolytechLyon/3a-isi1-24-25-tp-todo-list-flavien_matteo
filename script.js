e2809ff669cf1a1b1764c65a186b73b16e38c711
matteo <matteochauchet974@gmail.com>
2024-11-27 15:04:51 +0100

    test

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('new-todo-item-add');
+button.addEventListener("click", fct);
+function fct(event){
+    const button1 = document.createElement('new-todo-item-add');
+}
